bee7bab0bc1226d9bd97ceb51d722f6b49365c71
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2025-02-26 15:14:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img id='development' src='https://www.ccj.ufpb.br/dcx/contents/imagens/figuras/output_zvmmKf.gif/@@images/image.gif'></img>
+    </>
+  )
+}
+
+export default App
